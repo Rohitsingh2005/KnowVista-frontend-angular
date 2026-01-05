f33f14ab2af3f7ccc4516c7933709accfea8e542
RohitSingh <thakurrohit0811@gmail.com>
2026-01-05 22:17:52 +0530

    Initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,25 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './layout/navbar/navbar';
+import { Footer } from './layout/footer/footer';
+
+@NgModule({
+  declarations: [
+    App,
+    Navbar,
+    Footer,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
