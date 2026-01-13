f84f7804989e1cacc2e48688a6c9a3c03b59f539
RohitSingh <thakurrohit0811@gmail.com>
2026-01-13 23:55:57 +0530

    Implemented home

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,23 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {  NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     App,
+    Login,
+    Register,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CommonModule,
+    
+   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection()
